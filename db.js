190323f32a2ba62cd4fb47cf35075affe008ba8e
Will Johnson <wy.johnson96@gmail.com>
2021-10-05 21:32:13 -0500

    commented out mongodb, not functioning yet

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 /*
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/student-list");
+mongoose.connect("mongodb://localhost/results");
 
 mongoose.connection.on('open', () => {
     console.log("Connected to mongodb");
