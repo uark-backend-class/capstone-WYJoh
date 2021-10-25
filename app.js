79529b0483f433615a84d744d7c26bcb462ac0e8
Will Johnson <wy.johnson96@gmail.com>
2021-10-25 00:37:23 -0500

    env, last 5 category, reset button, shrink form distance

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const routes = require('./routes');
 const exphbs = require('express-handlebars');
+
 // require('./db');
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+// app.use(express.urlencoded());
 app.use(express.static("./style"));
 app.use(routes);
 
